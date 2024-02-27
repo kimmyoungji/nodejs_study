700680e507d288c3f0590cdb213e95ab55f2cea4
[AI9] 김명지 <kmjoyit21045@gmail.com>
2024-02-28 00:13:05 +0900

    chapter3 따라하기

--- chapter03/code3-5-refactoring-router-server.js
+++ chapter03/code3-5-refactoring-router-server.js
@@ -6,16 +6,14 @@ const server = http.createServer((req,res)=>{
     const path = url.parse(req.url, true).pathname; // true는 쿼리파람도 함께 파싱할지 여부를 결정
     res.setHeader("Content-Type","text/html");
     
-    if ( path === "/user" ) {
-        user(req,res)
-    }else if ( path === "/feed" ) {
-        feed(req, res)
+    if ( path in urlMap ) {
+        urlMap[path](req,res);
     }else {
         notFound(req,res)
     }
 })
 
-server.listen("3001",()=>{
+server.listen("3002",()=>{
     console.log("라우터를 만들어보자!")
 })
 
@@ -30,9 +28,15 @@ const feed = (res,req)=>{
     <li>picture2</li>
     <li>picture3</li>
     </ul>`)
-} 
+}
 
 const notFound = (req,res)=>{
     res.statusCode = 404;
     res.end("404 page not found");
+}
+
+const urlMap = {
+    "/": (req,res) => res.end("Home"),
+    "/user": user,
+    "/feed": feed
 }
