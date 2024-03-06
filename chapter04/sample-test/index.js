a03f1f41eadc7673c45a50e82dda1a091af30621
[AI9] 김명지 <kmjoyit21045@gmail.com>
2024-03-06 23:58:37 +0900

    chapter4

---
+++ chapter04/sample-test/index.js
@@ -0,0 +1,6 @@
+const cal = require('sample-package')
+
+console.log(cal.add(1,2))
+console.log(cal.sub(1,2))
+console.log(cal.multi(1,2))
+console.log(cal.div(1,2))
---
+++ chapter04/test-npx/index.js
@@ -0,0 +1,5 @@
+function getRandomInt(min, max /* 주석도 포맷팅 해줍니다 */) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+console.log(getRandomInt(10, 20));
